932e99208bdcdef16bb882e2cd7163983a56efe8
ckcy1 <ckcy1@qq.com>
2020-12-07 15:22:04 +0800

    引用 goto

--- TestMyAccount.go
+++ TestMyAccount.go
@@ -11,6 +11,7 @@ func main() {
 		fmt.Println("                       第二级菜单")
 		fmt.Println("                       第三级菜单")
 		fmt.Println("                       第四级菜单")
+		fmt.Println("                       5.退出菜单")
 		fmt.Println("请你输入菜单：")
 		fmt.Scan(&k)
 		switch k {
@@ -22,8 +23,13 @@ func main() {
 			fmt.Println("菜单一3")
 		case "4":
 			fmt.Println("菜单一4")
+		case "5":
+			goto breakTag
 		default:
 			fmt.Println("你输入无效！")
 		}
 	}
+breakTag:
+	fmt.Println("退出")
+
 }
