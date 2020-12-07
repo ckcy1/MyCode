01d68a186ba7c360a3d357f71f8b6f8f0df378bd
ckcy1 <ckcy1@qq.com>
2020-12-07 09:43:07 +0800

    for循环

---
+++ TestMyAccount.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		fmt.Println(".........................主菜单.........................")
+		fmt.Println("                       第一级菜单")
+		fmt.Println("                       第二级菜单")
+		fmt.Println("                       第三级菜单")
+		fmt.Println("                       第四级菜单")
+	}
+}
