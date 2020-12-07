0ae3f69fbdaf074298cc1bd4112647c297d14cbb
ckcy1 <ckcy1@qq.com>
2020-12-07 11:20:06 +0800

    test

--- TestMyAccount.go
+++ TestMyAccount.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var k string
+
 func main() {
 	for {
 		fmt.Println(".........................主菜单.........................")
@@ -9,5 +11,7 @@ func main() {
 		fmt.Println("                       第二级菜单")
 		fmt.Println("                       第三级菜单")
 		fmt.Println("                       第四级菜单")
+		fmt.Println("请你输入菜单：")
+		fmt.Scan(k)
 	}
 }
---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var M string
+
+func main() {
+
+	fmt.Println("请输入：44142419961208349X")
+	fmt.Scan(M)
+	fmt.Println(M)
+}
