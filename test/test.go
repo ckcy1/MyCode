6efe50970e92dd4aa9ee3ebc7c6a5a57905b43ad
ckcy1 <ckcy1@qq.com>
2020-12-09 11:00:15 +0800

    退出

--- test/test.go
+++ test/test.go
@@ -24,7 +24,6 @@ func newaccount(k string, note string, pay float64,
 
 func (a account) menu() {
 
-	k := a.k
 	for {
 		fmt.Println("\n---------------------家庭收支记账软件-----------------------------")
 		fmt.Println("                       1.收支明细")
@@ -32,16 +31,16 @@ func (a account) menu() {
 		fmt.Println("                       3.登记支出")
 		fmt.Println("                       4.退    出")
 		fmt.Printf("请选择（1-4）：")
-		fmt.Scan(&k)
-		switch k {
+		fmt.Scan(&a.k)
+		switch a.k {
 		case "1":
 
 		case "2":
 			a.shouru()
 		case "3":
-
+			a.zhichu()
 		case "4":
-
+a.exit()
 		default:
 			fmt.Println("你输入无效！")
 		}
@@ -60,6 +59,35 @@ func (a *account) shouru() {
 	a.str += fmt.Sprintf("\n收入\t\t%v\t\t%v\t\t%v", a.money, a.pay, a.note)
 	a.flag = false
 
+}
+func (a *account) zhichu() {
+	fmt.Println("本次支出金额：")
+	fmt.Scan(&a.pay)
+	if a.pay > a.money {
+		fmt.Println("余额不足")
+		//break
+	} else {
+		a.money -= a.pay
+		fmt.Printf("本次支出说明：")
+		fmt.Scan(&a.note)
+		// 字符串的拼接
+		a.str += fmt.Sprintf("\n支出\t\t%v\t\t%v\t\t%v", a.money, a.pay, a.note)
+		a.flag = false
+	}
+
+}
+func (a *account) exit() {
+	for {
+		fmt.Printf("你确定要退出y/n?")
+		fmt.Scan(&a.k)
+		if a.k == "y" {
+			goto breakTag
+		} else if a.k == "n" {
+			break
+		}
+	breakTag:
+		fmt.Println("退出")
+	}
 }
 func main() {
 	a := newaccount("", "", 3.0, 100.0, "", false)
