bc2f7639ac5725ca6b09a00af53665ce7b4bbae6
ckcy1 <ckcy1@qq.com>
2020-12-09 10:42:15 +0800

    方法

--- test/test.go
+++ test/test.go
@@ -2,19 +2,67 @@ package main
 
 import "fmt"
 
-// var m string
+type account struct {
+	k     string  //输入数字
+	note  string  //说明
+	pay   float64 //收支金额
+	money float64 // 账户金额
+	str   string  // \n收支\t账户金额\t收支金额\t说明 “拼接字符”
+	flag  bool    // 定义一个变量 ，记录是否有收支
+}
 
-// func main() {
+func newaccount(k string, note string, pay float64,
+	money float64, str string, flag bool) *account {
+	return &account{
+		k:     k,
+		note:  note,
+		pay:   pay,
+		money: money,
+		str:   str,
+		flag:  flag}
+}
 
-// 	fmt.Println("请输入：")
-// 	fmt.Scan(&m)
-// 	fmt.Println(m)
-// }
+func (a account) menu() {
+
+	k := a.k
+	for {
+		fmt.Println("\n---------------------家庭收支记账软件-----------------------------")
+		fmt.Println("                       1.收支明细")
+		fmt.Println("                       2.登记收入")
+		fmt.Println("                       3.登记支出")
+		fmt.Println("                       4.退    出")
+		fmt.Printf("请选择（1-4）：")
+		fmt.Scan(&k)
+		switch k {
+		case "1":
+
+		case "2":
+			a.shouru()
+		case "3":
+
+		case "4":
+
+		default:
+			fmt.Println("你输入无效！")
+		}
+	}
+	// breakTag:
+	// 	fmt.Println("退出")
+
+}
+func (a *account) shouru() {
+	fmt.Println("本次收入金额：")
+	fmt.Scan(&a.pay)
+	a.money += a.pay
+	fmt.Printf("本次收入说明：")
+	fmt.Scan(&a.note)
+	// 字符串的拼接
+	a.str += fmt.Sprintf("\n收入\t\t%v\t\t%v\t\t%v", a.money, a.pay, a.note)
+	a.flag = false
+
+}
 func main() {
-	var a *int
-	a = new(int)
-
-	fmt.Println(*a)
-	fmt.Println(a)
+	a := newaccount("", "", 3.0, 100.0, "", false)
+	a.menu()
 
 }
