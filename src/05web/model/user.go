9054d62dcac0f899fdb11162fab6d0824bbc73d4
ckcy1 <ckcy1@qq.com>
2021-01-07 21:12:39 +0800

    golang

--- src/05web/model/user.go
+++ src/05web/model/user.go
@@ -5,25 +5,74 @@ import (
 	"fmt"
 )
 
-type  User struct {
-	ID int
-	Username string
-	Password string
-	Email string
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
 
-func (u *User) AddUser()error  {
-// 写SQL语句
-	sqlStr :="insert into users(username,password,email)values (?,?,?)"
-	stmt,err :=utils.Db.Prepare(sqlStr)
+func (u *User) AddUser() error {
+	// 写SQL语句
+	sqlStr := "insert into users(username,password,email)values (?,?,?)"
+	stmt, err := utils.Db.Prepare(sqlStr)
 	if err != nil {
-		fmt.Println("预编译出现异常：",err)
+		fmt.Println("预编译出现异常：", err)
 		return err
 	}
-	_,err = stmt.Exec(u.Username, u.Password, u.Email)
+	_, err = stmt.Exec(u.Username, u.Password, u.Email)
 	if err != nil {
-		fmt.Println("执行出现异常：",err)
+		fmt.Println("执行出现异常：", err)
 		return err
 	}
 	return nil
+}
+func (u *User) GetUserById() (*User, error) {
+	sqlStr := "select id,username,password,email from users where id =?"
+	row := utils.Db.QueryRow(sqlStr, u.ID)
+	var (
+		id       int
+		username string
+		password string
+		email    string
+	)
+	err := row.Scan(&id, &username, &password, &email)
+	if err != nil {
+		return nil, err
+	}
+	user := &User{
+		ID:       id,
+		Username: username,
+		Password: password,
+		Email:    email,
+	}
+	return user, err
+}
+func (u *User) GetUsers() ([]*User, error) {
+	sqlStr := "select id,username,password,email from users"
+	rows, err := utils.Db.Query(sqlStr)
+	if err != nil {
+		return nil, err
+	}
+	var users []*User
+	for rows.Next() {
+		var (
+			id       int
+			username string
+			password string
+			email    string
+		)
+		err := rows.Scan(&id, &username, &password, &email)
+		if err != nil {
+			return nil, err
+		}
+		user := &User{
+			ID:       id,
+			Username: username,
+			Password: password,
+			Email:    email,
+		}
+		users = append(users, user)
+	}
+	return users, nil
 }
--- src/05web/model/user_test.go
+++ src/05web/model/user_test.go
@@ -5,16 +5,35 @@ import (
 	"testing"
 )
 
-//func TestUser(t *testing.T) {
-//	fmt.Println("开始测试USER中的相关方法")
-//	t.Run("测试添加用户：",TestAddUser)
-//}
-func TestAddUser(t *testing.T) {
+func TestUser(t *testing.T) {
+	fmt.Println("开始测试USER中的相关方法")
+	t.Run("测试添加用户：", testGetUsers)
+}
+func testAddUser(t *testing.T) {
 	fmt.Println("测试添加用户：")
-	User:=&User{
-		Username: "admin",
-		Password: "123",
-		Email:    "admin@qq.com",
+	User := &User{
+		Username: "admin11",
+		Password: "12311",
+		Email:    "admin11@qq.com",
 	}
 	User.AddUser()
+}
+func testGetUserById(t *testing.T) {
+	fmt.Println("查询用户：")
+	user := User{
+		ID: 3,
+	}
+	u, err := user.GetUserById()
+	if err != nil {
+		fmt.Println("出错拉！！！")
+	}
+	fmt.Printf("我的ID：%d,姓名：%s,密码：%s,邮箱：%s.\n", u.ID, u.Username, u.Password, u.Email)
+}
+func testGetUsers(t *testing.T) {
+	fmt.Println("查询多用户")
+	user := &User{}
+	u, _ := user.GetUsers()
+	for k, v := range u {
+		fmt.Printf("第%v个用户是%v:\n", k+1, v)
+	}
 }
